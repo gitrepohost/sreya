c023293d0a2686d708c655f49d5eef86f57a8ede
Shubham Raj <shubhamraj@Shubhams-MacBook-Pro.local>
2025-09-17 00:31:33 +0530

    asd

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import { clear } from "console";
 
 const app = express();
 app.use(express.json());
@@ -62,7 +61,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen(port, "127.0.0.1", () => {clear
+  server.listen(port, "127.0.0.1", () => {
     log(`serving on port ${port}`);
   });
 })();
